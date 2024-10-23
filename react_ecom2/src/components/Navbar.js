fee137a46b0c7d3033b9e3a6d12e238800ce8b37
gargsona7130 <gargsonal7130@gmail.com>
2024-10-23 13:29:00 +0530

    assets n navbar

---
+++ react_ecom2/src/components/Navbar.js
@@ -0,0 +1,24 @@
+import React,{useState} from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+    const [click,setClick] = useState(false);
+  return (
+    <>
+    <nav className="navbar">
+    <div className="navbar-container">
+        <Link to="/" className="navbar-logo">
+        SONAL
+        </Link>
+        <div className="menu-icon">
+            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+
+        </div>
+
+     </div>
+    </nav>
+    </>
+  )
+}
+
+export default Navbar
