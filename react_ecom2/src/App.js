a44233802e9b8a6e4a68a42dc45ec25bbb62d390
gargsona7130 <gargsonal7130@gmail.com>
2024-10-24 13:54:59 +0530

    Navbar

--- react_ecom2/src/App.js
+++ react_ecom2/src/App.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Switch, Route  } 
-from 'react-router-dom';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <>
-    <Router>
-       <Navbar />
-       <Switch>
-        <Route path='/' exact />
-       </Switch>
-       </Router>
-      </>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' exact />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
 export default App;
+
--- react_ecom2/src/components/Navbar.js
+++ react_ecom2/src/components/Navbar.js
@@ -1,8 +1,13 @@
-import React,{useState} from 'react'
-import { Link } from 'react-router-dom'
+import React,{useState} from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     const [click,setClick] = useState(false);
+    const [ button, setButton ] = useState(true);
+
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
   return (
     <>
     <nav className="navbar">
@@ -10,10 +15,37 @@ const Navbar = () => {
         <Link to="/" className="navbar-logo">
         SONAL
         </Link>
-        <div className="menu-icon">
+        <div className="menu-icon" onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
-
         </div>
+        <ul className={click ? 'nav-menu active' : 'nav-menu'}> 
+            <li className='nav-item'>
+                <Link to='/' className='nav-Links' onClick={closeMobileMenu}>
+                Home
+                </Link>
+            </li>
+
+            <li className='nav-item'>
+                <Link to='/services' className='nav-Links' onClick={closeMobileMenu}>
+                Services
+                </Link>
+            </li>
+
+            <li className='nav-item'>
+                <Link to='/products' className='nav-Links' onClick={closeMobileMenu}>
+                Products
+                </Link>
+            </li>
+
+            <li className='nav-item'>
+                <Link to='/sign-up' className='nav-Links-mobile' onClick={closeMobileMenu}>
+                 Sign Up
+                </Link>
+            </li>
+        </ul>
+
+    {button && <Button buttonStyle='btn--outline'> SIGN UP 
+        </Button>}
 
      </div>
     </nav>
