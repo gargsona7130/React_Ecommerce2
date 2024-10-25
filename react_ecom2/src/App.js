4f1365655de445ace85dc074584753d2ea8b338a
gargsona7130 <gargsonal7130@gmail.com>
2024-10-25 17:43:38 +0530

    Hero section

--- react_ecom2/src/App.js
+++ react_ecom2/src/App.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Home from './components/pages/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path='/' exact />
-        </Routes>
-      </Router>
-    </>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
+
 
